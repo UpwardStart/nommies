f12e905c069fe06c32340a73e0fdecba1c5b0906
SharjeelSIqbal <SharjeelSIqbal@gmail.com>
2022-04-04 20:17:03 -0700

    Added header and footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+const Footer = props => {
+  return (
+    <footer>
+        <div>
+          Something in the footer
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/header.jsx
@@ -0,0 +1,23 @@
+const Header = props => {
+  return (
+    <header>
+      <div>
+        <img src="" alt="some image" />
+      </div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Recipes</li>
+          <li>Filter</li>
+        </ul>
+        <label>
+          <input type="text" /> Search
+        </label>
+      </nav>
+
+    </header>
+  )
+
+}
+
+export default Header
